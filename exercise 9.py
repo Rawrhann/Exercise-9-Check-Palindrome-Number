66463d9d8652085a7ed118d80e08807a77f9b3ba
Ching <marcrohannmching@iskolarngbayan.pup.edu.ph>
2024-01-23 01:42:29 +0800

    creating a function

--- exercise 9.py
+++ exercise 9.py
@@ -2,17 +2,22 @@
 #Write a program to check if the given number is a palindrome number.
 #A palindrome number is a number that is the same after reverse. For example, 545, is the palindrome numbers
 
+def palindrome(number):
+    original_number = number
+     
 
-number = 12321
+    first_digit = (number % 10) * (10000)
+    second_digit = (number % 100) // 10 * 1000
+    third_digit = (number % 1000) // 100 * 100
+    fourth_digit = (number % 10000) // 1000 * 10
+    fifth_digit = (number % 100000) // 10000
+    new_number = first_digit+second_digit+third_digit+fourth_digit+fifth_digit  
 
-first_digit = (number % 10) * (10000)
-second_digit = (number % 100) // 10 * 1000
-third_digit = (number % 1000) // 100 * 100
-fourth_digit = (number % 10000) // 1000 * 10
-fifth_digit = (number % 100000) // 10000
-new_number = first_digit+second_digit+third_digit+fourth_digit+fifth_digit
 
-if number == new_number:
-    print(number, "is a palindrome")
-else:
-    print(number, "is not a palindrome")
+    if original_number == new_number:
+        print("the number", original_number, "is a palindrome")
+    else:
+        print("the number", original_number, "is not a palindrome")
+
+palindrome(12345)
+palindrome(12321)
