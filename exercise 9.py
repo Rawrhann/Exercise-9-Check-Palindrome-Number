aab94891419e825f6e14896297c0cc745557e7ed
Ching <marcrohannmching@iskolarngbayan.pup.edu.ph>
2024-01-23 01:49:00 +0800

    finished code

--- exercise 9.py
+++ exercise 9.py
@@ -5,14 +5,11 @@
 def palindrome(number):
     original_number = number
      
-
-    first_digit = (number % 10) * (10000)
-    second_digit = (number % 100) // 10 * 1000
-    third_digit = (number % 1000) // 100 * 100
-    fourth_digit = (number % 10000) // 1000 * 10
-    fifth_digit = (number % 100000) // 10000
-    new_number = first_digit+second_digit+third_digit+fourth_digit+fifth_digit  
-
+    new_number = 0
+    while number > 0:
+        reminder = number % 10
+        new_number = (new_number * 10) + reminder
+        number = number // 10
 
     if original_number == new_number:
         print("the number", original_number, "is a palindrome")
@@ -20,4 +17,4 @@ def palindrome(number):
         print("the number", original_number, "is not a palindrome")
 
 palindrome(12345)
-palindrome(12321)
+palindrome(1234321)
